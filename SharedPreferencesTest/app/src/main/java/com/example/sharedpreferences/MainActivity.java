7a037eb5056f91b5b7e271adc2beae2a68ab1a05
navyxue <navy_xue@126.com>
2019-04-29 17:25:53 +0800

    Android SharedPreferences存储

---
+++ SharedPreferencesTest/app/src/main/java/com/example/sharedpreferences/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.sharedpreferences;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "d-MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.save_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
+                editor.putString("name", "Tom");
+                editor.putInt("age", 20);
+                editor.putBoolean("married", false);
+                editor.apply();
+            }
+        });
+        Button restoreButton = (Button) findViewById(R.id.restore_data);
+        restoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences pref = getSharedPreferences("data", MODE_PRIVATE);
+                String name = pref.getString("name", "");
+                int age = pref.getInt("age", 0);
+                boolean married = pref.getBoolean("married", false);
+                Log.d(TAG, "name is " + name);
+                Log.d(TAG, "age is " + age);
+                Log.d(TAG, "married is " + married);
+            }
+        });
+    }
+}
