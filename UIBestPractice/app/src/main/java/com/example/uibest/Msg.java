67703f5ee99c85f258374fb002e18683b8eb2fc5
navyxue <navy_xue@126.com>
2019-04-29 17:16:43 +0800

    Android UI最佳实践之聊天界面

---
+++ UIBestPractice/app/src/main/java/com/example/uibest/Msg.java
@@ -0,0 +1,29 @@
+package com.example.uibest;
+
+public class Msg {
+    public static final int TYPE_RECEIVED = 0;
+    public static final int TYPE_SENT = 1;
+
+    private String content;
+    private int type;
+
+    public Msg(String content, int type) {
+        this.content = content;
+        this.type = type;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+    public void  setContent(String content) {
+        this.content = content;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+}
