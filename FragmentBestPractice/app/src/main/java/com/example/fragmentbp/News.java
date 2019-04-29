1394436385780588f6ed367fdca636816307b319
navyxue <navy_xue@126.com>
2019-04-29 17:23:11 +0800

    Android Fragment帧布局最佳实践

---
+++ FragmentBestPractice/app/src/main/java/com/example/fragmentbp/News.java
@@ -0,0 +1,27 @@
+package com.example.fragmentbp;
+
+/**
+ * 新闻类
+ * @author xuehj
+ * @since 2019-03-02
+ */
+public class News {
+    private String title;
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
